2b53e49f8bdab7332167a64e9c12807f6984d4ba
Taylor Covington <tcovingtondev@gmail.com>
2019-08-29 20:56:58 -0600

    controller

--- app/controllers/billboards_controller.rb
+++ app/controllers/billboards_controller.rb
@@ -1,13 +1,25 @@
 class BillboardsController < ApplicationController
+  before_action :set_billboard, only: [:show, :edit]
+
   def index
+    @billboards = Billboard.all
   end
 
   def show
+    
   end
 
   def new
   end
 
   def edit
+    
   end
+
+  private
+
+  def set_billboard
+    @billboard = Billboard.find(params[:id])
+  end
+
 end
